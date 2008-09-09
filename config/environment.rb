941d48144561fca090dec9120da36fface24a38a
Trevor Turk <trevorturk@yahoo.com>
2008-09-08 23:02:11 -0500

    rescue exception that will be raised on initial migration (whoops)

--- config/environment.rb
+++ config/environment.rb
@@ -10,15 +10,15 @@ Rails::Initializer.run do |config|
 end
 
 # Your cookie sessions configuration is read from config/database.yml or automatically generated
-if Configurator[:session_key].blank? && Configurator[:session_secret].blank?
-  db = YAML.load_file('config/database.yml')
-  if db[RAILS_ENV]['session_key'] && db[RAILS_ENV]['secret']
-    Configurator[:session_key] = db[RAILS_ENV]['session_key']
-    Configurator[:session_secret] = db[RAILS_ENV]['secret']
-  else
-    Configurator[:session_key] = Rails::SecretKeyGenerator.new(rand).generate_secret
-    Configurator[:session_secret] = Rails::SecretKeyGenerator.new(rand).generate_secret
-  end
+db = YAML.load_file('config/database.yml')
+if db[RAILS_ENV]['session_key'] && db[RAILS_ENV]['secret']
+  session_key    = db[RAILS_ENV]['session_key']
+  session_secret = db[RAILS_ENV]['secret']
+else
+  session_key    = Rails::SecretKeyGenerator.new(rand).generate_secret
+  session_secret = Rails::SecretKeyGenerator.new(rand).generate_secret
 end
-ActionController::Base.session_options[:session_key] = Configurator[:session_key]
-ActionController::Base.session_options[:secret] = Configurator[:session_secret]
+ActionController::Base.session_options[:session_key] = session_key
+ActionController::Base.session_options[:secret] = session_secret
+# Store cookie sessions config, rescue for initial running of configurator migration
+begin; Configurator[:session_key] = session_key; Configurator[:session_secret] = session_secret; rescue; end
